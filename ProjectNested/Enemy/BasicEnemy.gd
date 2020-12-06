e06f560a71fc9ad1cf8a4427404f4922a6f1b7b5
SkyanSam <samuelwiggins15@gmail.com>
2020-12-06 02:56:22 -0500

    Player Animations, Wall Jump, Player HP, modifying enemies
    still need to implement dreamcatcher ui for player hp.

--- ProjectNested/Enemy/BasicEnemy.gd
+++ ProjectNested/Enemy/BasicEnemy.gd
@@ -4,11 +4,16 @@ var direction = 1;
 var previous_direction = 0;
 var hp = 0;
 export var speed = 200;
+export var freak_out_speed_multiplier = 2;
 export var minX = 0;
 export var maxX = 200;
 export var startHP = 3;
 
 var touching_player = false
+var freak_out = false
+var attacking = false
+onready var player = get_tree().get_root().get_node("Node2D").get_node("player")
+
 # Called when the node enters the scene tree for the first time.
 func getDir():
 	match(direction):
@@ -23,43 +28,56 @@ func setDir(value):
 
 func _ready():
 	position.x = minX;
+	hp = startHP
 	
 func _physics_process(delta):
 	if (position.x < minX):
 		setDir(1);
 	elif (position.x > maxX):
 		setDir(-1);
-	position += Vector2(speed * direction * delta, 0);
+	var velocity = Vector2(speed * direction * delta, 0);
+	if (freak_out):
+		velocity.x *= freak_out_speed_multiplier;
+	position += velocity;
+	
+	if (touching_player && player.get_is_attacking()):
+		lose_health()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
 
 func _on_Area2D_body_entered(body):
-	if (body.name == "Player"):
+	if (body.name == "player"):
+		attack()
 		touching_player = true;
 		
 
 func _on_Area2D_body_exited(body):
-	if (body.name == "Player"):
+	if (body.name == "player"):
 		touching_player = false;
 
 func attack():
-	setDir(0);
-	# NOT IMPLEMENTED
-	pass
+	if (!attacking):
+		attacking = true;
+		setDir(0);
+		player.lose_hp()
+		yield(get_tree().create_timer(1), "timeout");
+		setDir(getDir());
+		yield(get_tree().create_timer(5), "timeout");
+		attacking = false;
 
 func lose_health():
-	
-	hp -= 1;
-	if (hp < 0):
-		die();
-	
-	setDir(getDir() * -1);
-	yield(get_tree().create_timer(1), "timeout");
-	setDir(0);
-	yield(get_tree().create_timer(1), "timeout");
-	setDir(getDir() * -2);
+	if (!freak_out):
+		freak_out = true;
+		
+		hp -= 1;
+		if (hp < 0):
+			die();
+		
+		
+		yield(get_tree().create_timer(1), "timeout");
+		freak_out = false;
 
 func die():
 	queue_free();
--- ProjectNested/KinematicBody2D.gd
+++ ProjectNested/KinematicBody2D.gd
@@ -1,11 +1,25 @@
 extends KinematicBody2D
 
 export (int) var speed = 300
-export (int) var jump_speed = -300
-export (int) var gravity = 500
-
+export (int) var jump_speed = -450
+export (int) var gravity = 900
+export (int) var startHP = 3;
+export (float) var hpCooldownTime = 0.3
+var hp = 0
+var hpCooldown = false 
+var isAttacking = false
+func get_is_attacking():
+	return isAttacking;
 var velocity = Vector2.ZERO
+var last_wall_direction = 0;
 
+onready var left_wall_raycasts = $WallRaycasts/LeftWallRaycasts
+onready var right_wall_raycasts = $WallRaycasts/RightWallRaycasts
+
+func _ready():
+	hp = startHP
+	
+# PHYSICS
 func get_input():
 	velocity.x = 0
 	if Input.is_action_pressed("right"):
@@ -15,9 +29,82 @@ func get_input():
 
 func _physics_process(delta):
 	get_input()
+	var wall_direction = get_wall_direction()
+	print("wall direction" + str(wall_direction))
 	velocity.y += gravity * delta
-	velocity = move_and_slide(velocity, Vector2.UP)
-	if Input.is_action_just_pressed("up"):
-		if is_on_floor():
+	
+	if is_on_floor():
+		if Input.is_action_just_pressed("up"):
 			velocity.y = jump_speed
+			animation_play("jump")
+		else:
+			if (abs(velocity.x) > 0.25):
+				animation_play("walk")
+				$AnimatedSprite.flip_h = velocity.x < 0
+			else:
+				animation_play("idle")
+			
+	elif wall_direction != 0:
+		animation_play("jump")
+		print("WALL")
+		$AnimatedSprite.flip_h = wall_direction > 0
+		var right = Input.is_action_pressed("right") && wall_direction == -1;
+		var left = Input.is_action_pressed("left") && wall_direction == 1;
+		print("r" + str(right))
+		print("l" + str(left))
+		if (right || left):
+			print("WALL JUMP")
+			velocity.y = jump_speed
+			last_wall_direction = 0;
+			
+		
+	if Input.is_action_just_pressed("attack"):
+		animation_play("attack")
+		isAttacking = true;
+	
+	velocity = move_and_slide(velocity, Vector2.UP)
 
+
+func _on_AnimatedSprite_animation_finished():
+	isAttacking = false;
+
+func lose_hp():
+	if (!hpCooldown):
+		hpCooldown = true;
+		hp -= 1;
+		if (hp <= 0):
+			game_over();
+			
+		yield(get_tree().create_timer(hpCooldownTime), "timeout");
+		hpCooldown = false;
+		
+func game_over():
+	pass
+	
+func animation_play(clip):
+	if (!isAttacking):
+		$AnimatedSprite.play(clip)
+
+func check_is_valid_wall(wall_raycasts) -> bool:
+	for raycast in wall_raycasts.get_children():
+		if raycast.is_colliding():
+			return true
+			#var dot = acos(Vector2.UP.dot(raycast.get_collision_normal()))
+			#if dot > PI * 0.35 && dot < PI * 0.55:
+				#return true
+	return false
+
+func get_wall_direction():
+	var is_near_wall_left = check_is_valid_wall(left_wall_raycasts)
+	var is_near_wall_right = check_is_valid_wall(right_wall_raycasts)
+	
+	if is_near_wall_left && is_near_wall_right:
+		if (velocity.x > 0):
+			return 1;
+		elif (velocity.x < 0):
+			return -1;
+		else:
+			return 0;
+	else:
+		return -int(is_near_wall_left) + int(is_near_wall_right)
+				
